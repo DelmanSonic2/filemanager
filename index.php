4cc36b3937913d3e5b787078aff0016da9b69a7d
Delmansonic2 <delman.dolomanov@gmail.com>
2022-04-14 16:35:02 +0300

    автообновление страницы

--- index.php
+++ index.php
@@ -27,18 +27,18 @@
     </div>
     <div class="dn">
         <div class="add_file_modal">
-            <form action="" class=add_file_form>
+            <form action="" method="post" class=add_file_form>
                 <label for="input_template">Путь</label>
                 <input id=input_template class="input_template" name=input_template type="text">
                 <button type="submit" value="Добавить файл" name=submit>Добавить файл</button>
             </form>
-            <form action="" class=rename_file_form>
+            <form action=""  method="post" class=rename_file_form>
                 <label for="rename_template">Название</label>
                 <input type="text" class=rename_template_null name="rename_template_null" style="display:none;">
                 <input id=rename_template class="rename_template" name=rename_template type="text">
                 <button type="submit" value="Добавить файл" name=submit>Переименовать</button>
             </form>
-            <form action="" class=add_folder_form>
+            <form action=""  method="post" class=add_folder_form>
                 <label for="input_templat_foldere">Путь</label>
                 <input id=input_template_folder class="input_template_folder" name=input_template_folder type="text">
                 <button type="submit" value="Добавить файл" name=submit>Добавить папку</button>
@@ -55,5 +55,8 @@ if (isset($_REQUEST['submit'])) {
         add_folder($_REQUEST['input_template_folder']);
     if (isset($_REQUEST['rename_template_null']) && isset($_REQUEST['rename_template']))
         rename_file($_REQUEST['rename_template_null'], $_REQUEST['rename_template']);
+        ?>
+        <script> window.location.href = './';</script>
+        <?php
 }
 ?>
